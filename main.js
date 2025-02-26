cf7a5c4776b8d3b54d8324631e71854e23c7855b
Flearly <teymurzadehuseyn28@gmail.com>
2025-02-27 00:30:23 +0300

    create the window

---
+++ main.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const {app, BrowserWindow} = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('./renderer/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
