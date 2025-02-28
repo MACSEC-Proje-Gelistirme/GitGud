72e1f8956e148d905648f2ce7d611cbf1b38168d
Flearly <teymurzadehuseyn28@gmail.com>
2025-02-28 03:28:42 +0300

    windows problems fixed and improve overall ui

--- electron.js
+++ electron.js
@@ -1,29 +1,47 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-const isDev = process.env.NODE_ENV === 'development' || !app.isPackaged;
-
 let mainWindow;
+const isDev = !app.isPackaged;
 
 function createWindow() {
+  console.log('createWindow function is called');
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
   const startURL = isDev
     ? 'http://localhost:3000'
     : `file://${path.join(__dirname, './build/index.html')}`;
-
+  
+  console.log('URL:', startURL);
+  
   mainWindow.loadURL(startURL);
+  
 
-  mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.webContents.on('did-fail-load', (_, errorCode, errorDescription) => {
+    console.log('page loading error:', errorCode, errorDescription);
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  console.log('Electron app is ready');
+  createWindow();
+});
+
+// Hata yakalama
+process.on('uncaughtException', (error) => {
+  console.log('uncaught Exception:', error);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
